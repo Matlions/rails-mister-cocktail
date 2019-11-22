2dbc541eb84c29a24d8ba77a58ef8df1cc4e7a17
Matlions <matthieurpeyronnet@gmail.com>
2019-11-22 15:08:23 +0100

    navbar body back ground

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,9 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+(function() {
+  this.App || (this.App = {});
+
+  App.cable = ActionCable.createConsumer();
+
+}).call(this);
